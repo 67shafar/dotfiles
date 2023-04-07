b1ed90a2e7c5b4f6831dd6d416d0e93d1a7b8293
Shane Farrar <farrar.shane@gmail.com>
2023-04-07 16:41:34 -0400

    Add undo tree and git plugins to neovim config

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,4 +2,12 @@ local telescope = require('telescope.builtin')
 
 -- Project Keys
 vim.keymap.set("n", "<leader>pf", telescope.find_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+  telescope.grep_string({ search = vim.fn.input("Grep > ") })
+end)
 
+-- File History
+vim.keymap.set("n", "<leader>fu", vim.cmd.UndotreeToggle)
+
+-- Git Commands
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
