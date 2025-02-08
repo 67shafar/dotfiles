4e5e0eafd55db009be4216bec810177285c890dd
Shane Farrar <sfarrar@onpointlearning.com>
2025-02-07 19:51:08 -0500

    Latest updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,47 +13,149 @@ end
 vim.opt.rtp:prepend(lazypath)
 ------------ END: PLUGIN MANAGEMENT --------------
 
-
 ------------ START: PLUGIN SETTINGS  ------------
 require("lazy").setup({
+  -- Common Libs
+  { 'nvim-lua/plenary.nvim', tag = 'v0.1.4' },
+
+  -- Themes
 	{ 'Mofiqul/vscode.nvim', lazy = false },
-	{ 'nvim-telescope/telescope.nvim', tag = '0.1.3', dependencies = { 'nvim-lua/plenary.nvim', tag = 'v0.1.3' } },
-  { 'nvim-telescope/telescope-file-browser.nvim', dependencies = { 'nvim-telescope/telescope.nvim' }, lazy = false },
-  { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons', opt = true } },
-	{ 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate", config = function() require("nvim-treesitter.configs").setup {
-        ensure_installed = { "markdown", "markdown_inline", "mermaid", "toml", "yaml", "typescript", "python", "java", "bash", "lua", "rust", "kotlin" },
-        highlight = { enable = true, }
-  } end },
-	{	'mbbill/undotree' },
-	{ 'lewis6991/gitsigns.nvim' },
-  { 'akinsho/toggleterm.nvim', config = true },
-	{ 'williamboman/mason.nvim' },
-	{ 'williamboman/mason-lspconfig.nvim', 'neovim/nvim-lspconfig' },
-  { 'mfussenegger/nvim-lint' },
-  { 'rshkarin/mason-nvim-lint' },
-  { 'stevearc/conform.nvim', events = { "BufReadPre", "BufNewFile" }, config =
-      function() require("conform").setup({ formatters_by_ft = { kotlin = { "ktlint" } }, }) end
+
+  -- Telescope
+	{
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim', tag = 'v0.1.4' },
+    lazy = false
   },
-  { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+
+  -- Highlighting Support
+	{
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function() require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "markdown", "markdown_inline", "mermaid",
+          "toml", "yaml", "typescript", "python",
+          "java", "bash", "lua", "rust", "kotlin"
+        },
+        highlight = { enable = true, }
+    } end
+  },
+  { 'VonHeikemen/lsp-zero.nvim', branch = 'v4.x' },
+
+  -- Git support
+	{ 'lewis6991/gitsigns.nvim', config = {} },
+
+  -- Package dependencies installer
+	{ 'williamboman/mason.nvim', config = {} },
+  { 'williamboman/mason-lspconfig.nvim'  },
+  {
+    'mfussenegger/nvim-lint',
+    'rshkarin/mason-nvim-lint',
+  },
+
+  -- Code Completion Support
+
   {	'neovim/nvim-lspconfig' },
 	{	'hrsh7th/cmp-nvim-lsp' },
 	{	'hrsh7th/nvim-cmp' },
-	{ "iamcco/markdown-preview.nvim", ft={"markdown"}, build = function() vim.fn["mkdp#util#install"]() end, },
+
+  -- Editor Features
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+  },
+  { 'akinsho/toggleterm.nvim', config = { direction = 'float' } },
   { 'nvim-tree/nvim-tree.lua' },
-  { 'romgrk/barbar.nvim' }
+  { 'romgrk/barbar.nvim' },
+  { 'windwp/nvim-autopairs', event = "InsertEnter", config = true },
+  { 'numToStr/Comment.nvim' },
+  { 'Shatur/neovim-session-manager' },
+  { 'ggandor/leap.nvim' },
+	{	'mbbill/undotree' },
+
+  -- LLM Features
+  { 'jackMort/ChatGPT.nvim', event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim", "folke/trouble.nvim" },
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "op read op://private/OpenAI-SF-Personal/credential --no-newline",
+        openai_params={
+          model = "gpt-4o-mini",
+          max_tokens = 128000,
+        },
+        openai_edit_params = {
+          model = "gpt-4o-mini",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens=128000,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+      })
+    end
+  },
 },{})
 
+--- Start LSP And Code Configuration ---
 local lsp_zero = require('lsp-zero')
-lsp_zero.on_attach(function(_, bufnr) lsp_zero.default_keymaps({buffer = bufnr}) end)
-
-require('mason').setup({})
-require('mason-nvim-lint').setup({ ensure_installed = { "ktlint" }, quiet_mode = true })
-require('mason-lspconfig').setup({
-  ensure_installed = { "lua_ls", "rust_analyzer", "kotlin_language_server" },
-  handlers = { lsp_zero.default_setup, },
+lsp_zero.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = function(_, bufnr)
+    lsp_zero.default_keymaps({buffer = bufnr})
+  end,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
-require('lspconfig').lua_ls.setup({ settings = { Lua = { diagnostics = { globals = {"vim"} } } } })
-require('toggleterm').setup { direction = 'float' }
+require('mason-lspconfig').setup({
+  ensure_installed = {"lua_ls", "pyright"}
+})
+
+require('lspconfig').lua_ls.setup({ settings = {
+  Lua = { diagnostics = { globals = {"vim"} } }
+} })
+
+require('lspconfig').pyright.setup { settings = {
+  pyright= { autoImportCompletions = true },
+  python = {
+    analysis = {
+      autoSearchPaths = true,
+      diagnosticMode = 'openFilesOnly',
+      useLibraryCodeForTypes = true,
+      typeCheckingMode = 'strict'
+    }
+  }
+}}
+
+local cmp = require('cmp')
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  snippet = {
+    expand = function(args)
+      -- You need Neovim v0.10 to use vim.snippet
+      vim.snippet.expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({}),
+})
+--- END LSP & Code Completion ---
+
+
+require('leap').create_default_mappings()
+require('Comment').setup()
+
+vim.treesitter.query.set(
+  "python",  -- Language to apply the query to
+  "injections",  -- Query type (injections)
+  [[
+  (string (string_content) @injection.content
+   ;; Check if the string contains the `#!bash` marker
+   (#match? @injection.content "^#!/bin/bash(.*)")
+   (#set! injection.language "bash"))
+  ]]
+)
 ----------- END: PLUGIN SETTINGS  ---------------
 
 
@@ -67,7 +169,7 @@ require('barbar').setup({sidebar_filetypes = { NvimTree = true, undotree = true 
 
 
 ------------ START: EDITOR SETTINGS ------------
-vim.opt.mouse = ""
+vim.opt.mouse = "a"
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
@@ -77,8 +179,11 @@ vim.opt.relativenumber = true
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = false
 vim.opt.timeoutlen=400
+vim.opt.textwidth=150
+vim.opt.colorcolumn='80'
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.cmd("set guifont=JetBrainsMono\\ Nerd\\ NFM:h10")
 ------------ END: EDITOR SETTINGS --------------
 
 
@@ -93,16 +198,21 @@ vim.keymap.set({"i","n","v","t"}, "<leader>ss", builtin.live_grep, {})
 vim.keymap.set({"i","n","v","t"}, "<leader>t", vim.cmd.ToggleTerm)
 vim.keymap.set({"i","n","v","t"}, "<leader>u", vim.cmd.UndotreeToggle)
 vim.keymap.set({"i","n","v","t"}, "<leader><leader>", "<ESC>", { silent = true })
-
+vim.keymap.set({"i","n","v","t"}, "<leader>a", vim.lsp.buf.code_action)
+vim.keymap.set({"i","n","v","t"}, "<leader>gd", vim.lsp.buf.definition)
+vim.keymap.set({"i","n","v","t"}, "<leader>b", "<Cmd>bn<CR>")
+vim.keymap.set({"v"}, "<leader>r", "y:%s/<C-r>\"//g<left><left>")
 -- Save buffer contents
 vim.keymap.set("n", "<D-s>", ":w<CR>")
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("i", "<D-s>", "<ESC>:w<CR>a")
 vim.keymap.set("i", "<C-s>", "<ESC>:w<CR>a")
 
+-- Close Buffer
+vim.keymap.set("n", "<leader>q", ":bd<CR>")
+
 -- Disable arrow keys
 vim.keymap.set({ "", "i" }, "<up>", "<nop>")
 vim.keymap.set({ "", "i" }, "<down>", "<nop>")
-vim.keymap.set({ "", "i" }, "<left>", "<nop>")
 vim.keymap.set({ "", "i" }, "<right>", "<nop>")
 ------------ END:  KEY REMAPPING-------------
